9fac68222445e64c56c3ab1900c819310022f6e7
Josh Wannebo <jwannebo@users.noreply.github.com>
2021-12-02 12:07:05 -0500

    Add files via upload

---
+++ aoc21_2b/src/main.rs
@@ -0,0 +1,51 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Lines};
+use std::path::Path;
+
+fn path_to_lines<P>(path: P) -> Result<Lines<BufReader<File>>, io::Error>
+where
+    P: AsRef<Path>,
+{
+    Ok(BufReader::new(File::open(path)?).lines())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut horizontal: isize = 0;
+    let mut depth: isize = 0;
+    let mut aim: isize = 0;
+    // let demo: Vec<Result<&str, ()>> = vec![
+    //     Ok("forward 5"),
+    //     Ok("down 5"),
+    //     Ok("forward 8"),
+    //     Ok("up 3"),
+    //     Ok("down 8"),
+    //     Ok("forward 2"),
+    // ];
+    //for line in demo {
+    for line in path_to_lines("input")? {
+        if let Ok(line) = line {
+            let tokens = line.split(' ').collect::<Vec<&str>>();
+            assert_eq!(tokens.len(), 2);
+            match (tokens[0], tokens[1].parse::<isize>()) {
+                ("forward", Ok(n)) => {
+                    horizontal += n;
+                    depth += aim * n;
+                    if depth < 0 {
+                        depth = 0;
+                    }
+                }
+                ("down", Ok(n)) => aim += n,
+                ("up", Ok(n)) => aim -= n,
+                _ => panic!("Invalid Input"),
+            }
+        }
+    }
+    println!(
+        "horizontal = {} depth = {}\n{}",
+        horizontal,
+        depth,
+        horizontal * depth
+    );
+    Ok(())
+}
