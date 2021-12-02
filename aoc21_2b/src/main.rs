072650b3ddd37f8d98040d406a9c2b2a0502ed1a
Josh Wannebo <jwannebo@users.noreply.github.com>
2021-12-02 15:06:35 -0500

    Clippy

--- aoc21_2b/src/main.rs
+++ aoc21_2b/src/main.rs
@@ -14,23 +14,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut horizontal: isize = 0;
     let mut depth: isize = 0;
     let mut aim: isize = 0;
-    
-    for line in path_to_lines("input")? {
-        if let Ok(line) = line {
-            let tokens = line.split(' ').collect::<Vec<&str>>();
-            assert_eq!(tokens.len(), 2);
-            match (tokens[0], tokens[1].parse::<isize>()) {
-                ("forward", Ok(n)) => {
-                    horizontal += n;
-                    depth += aim * n;
-                    if depth < 0 {
-                        depth = 0;
-                    }
+    for line in path_to_lines("input")?.flatten() {
+        let tokens = line.split(' ').collect::<Vec<&str>>();
+        assert_eq!(tokens.len(), 2);
+        match (tokens[0], tokens[1].parse::<isize>()) {
+            ("forward", Ok(n)) => {
+                horizontal += n;
+                depth += aim * n;
+                if depth < 0 {
+                    depth = 0;
                 }
-                ("down", Ok(n)) => aim += n,
-                ("up", Ok(n)) => aim -= n,
-                _ => panic!("Invalid Input"),
             }
+            ("down", Ok(n)) => aim += n,
+            ("up", Ok(n)) => aim -= n,
+            _ => panic!("Invalid Input"),
         }
     }
     println!(
