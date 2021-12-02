1322d83f223392fe125d6a5164d1e78a12e6415c
Josh Wannebo <jwannebo@users.noreply.github.com>
2021-12-02 15:04:26 -0500

    Clippy

--- aoc21_2a/src/main.rs
+++ aoc21_2a/src/main.rs
@@ -12,21 +12,19 @@ where
 fn main() -> Result<(), Error> {
     let mut horizontal = 0;
     let mut depth = 0;
-    for line in path_to_lines("input")? {
-        if let Ok(line) = line {
-            let tokens = line.split(' ').collect::<Vec<&str>>();
-            assert_eq!(tokens.len(), 2);
-            match (tokens[0], tokens[1].parse::<isize>()) {
-                ("forward", Ok(n)) => horizontal += n,
-                ("down", Ok(n)) => depth += n,
-                ("up", Ok(n)) => {
-                    depth -= n;
-                    if depth < 0 {
-                        depth = 0;
-                    };
-                }
-                _ => panic!("Invalid Input"),
+    for line in path_to_lines("input")?.flatten() {
+        let tokens = line.split(' ').collect::<Vec<&str>>();
+        assert_eq!(tokens.len(), 2);
+        match (tokens[0], tokens[1].parse::<isize>()) {
+            ("forward", Ok(n)) => horizontal += n,
+            ("down", Ok(n)) => depth += n,
+            ("up", Ok(n)) => {
+                depth -= n;
+                if depth < 0 {
+                    depth = 0;
+                };
             }
+            _ => panic!("Invalid Input"),
         }
     }
     println!(
